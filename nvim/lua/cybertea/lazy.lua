5aafdebd9e83c27c01b6407d94ead7e90459728c
CyberTea0X <mihailusov_778@mail.ru>
2024-08-21 02:33:23 +0300

    update .dotfiles

--- nvim/lua/cybertea/lazy.lua
+++ nvim/lua/cybertea/lazy.lua
@@ -63,6 +63,7 @@ require("lazy").setup({
             show_icons = true,
             leader_key = '<leader>a',        -- Recommended to be a single key
             buffer_leader_key = '<leader>m', -- Per Buffer Mappings
+            global_bookmarks = true,
         }
     },
     'mbbill/undotree',
