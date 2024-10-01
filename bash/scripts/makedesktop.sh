affb1aeca3f99a2a8b3ef91bd44925665e53c90e
CyberTea0X <mihailusov_778@mail.ru>
2024-10-01 15:48:44 +0300

    update .dotfiles

--- bash/scripts/makedesktop.sh
+++ bash/scripts/makedesktop.sh
@@ -17,11 +17,17 @@ read -p "Custom application name? (default:$filename? (Y/N): " confirm
 if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
     read -p "Enter application name: " appname
 fi
+read -p "Custom icon? (Y/N): " confirm 
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    read -p "Enter icon path or icon name" icon
+    icon="Icon=$icon"
+fi
 echo "\
 [Desktop Entry]
 Name=$appname
 Exec=$(realpath $filepath)
 Type=Application\
+$icon
 " > ~/.local/share/applications/${filename}.desktop
 chmod +x ~/.local/share/applications/${filename}.desktop
 echo "${filename}.desktop have been created in ~/.local/share/applications"
