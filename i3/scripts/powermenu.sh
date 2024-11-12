c65292c418bf676bb05e5179941409882680e433
CyberTea0X <mihailusov_778@mail.ru>
2024-11-12 12:28:05 +0300

    update .dotfiles

--- i3/scripts/powermenu.sh
+++ i3/scripts/powermenu.sh
@@ -1,4 +1,4 @@
 rofi \
   -show p \
   -modi p:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
-  -theme-str 'window {width: 8em;} listview {lines: 6;}'
+  -theme-str 'window {location: westsouth; margin: 0 0 50 0; orientaion: vertical; width: 8em;} listview {lines: 6;}'
