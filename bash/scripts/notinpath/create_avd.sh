a8f87cdd7817aee43554e28d244f939cb4664a26
CyberTea0X <mihaylusov92@gmail.com>
2025-06-11 04:01:00 +0300

    update .dotfiles

--- bash/scripts/notinpath/create_avd.sh
+++
@@ -1,1 +0,0 @@
-avdmanager create avd --name test -k "system-images;android-31;default;x86_64"
--- bash/scripts/notinpath/enable_syncthing.sh
+++
@@ -1,4 +0,0 @@
-# start sync service
-sudo systemctl start syncthing@$USER.service
-# autostart
-sudo systemctl enable syncthing@$USER.service
